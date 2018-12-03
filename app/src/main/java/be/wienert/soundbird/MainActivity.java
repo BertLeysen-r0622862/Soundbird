5831eb268ef0b137ad6e305a73a1f9e9605cf4a1
LAPTOP-GMI2185F\Bert Leysen <bert.leysen@ucll.be>
2018-12-03 20:25:18 +0100

    Cancel Button works (AddSoundActivity)

--- app/src/main/java/be/wienert/soundbird/MainActivity.java
+++ app/src/main/java/be/wienert/soundbird/MainActivity.java
@@ -3,6 +3,10 @@ package be.wienert.soundbird;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import be.wienert.soundbird.service.SoundBoardRestService;
 import be.wienert.soundbird.service.SoundBoardService;
@@ -10,15 +14,32 @@ import be.wienert.soundbird.service.SoundBoardService;
 public class MainActivity extends AppCompatActivity {
 
     private SoundBoardService service;
+    private Button toAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toAddButton = (Button) findViewById(R.id.toAddButton);
+        toAddButton.setOnClickListener(buttonListener);
+
         service = new SoundBoardRestService();
         new CreateSoundsTask(this).execute(service);
-        Intent intentAddFile = new Intent(this,AddButtonActivity.class);
-        startActivity(intentAddFile);
+
+        //Intent intentAddFile = new Intent(this,AddButtonActivity.class);
+        //startActivity(intentAddFile);
     }
+
+   private View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.toAddButton:
+                    startActivity(new Intent(MainActivity.this, AddButtonActivity.class));
+                    break;
+            }
+        }
+    };
 }
