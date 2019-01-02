3b3fb2215d2df67e8e965ae155f9fab874e35351
Bert Leysen <bert.leysen@ucll.be>
2019-01-02 17:31:50 +0100

    In app - Filesize error

--- app/src/main/java/be/wienert/soundbird/AddButtonActivity.java
+++ app/src/main/java/be/wienert/soundbird/AddButtonActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -14,7 +15,10 @@ import android.widget.EditText;
 import android.widget.GridLayout;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.util.concurrent.ThreadLocalRandom;
 
 import be.wienert.soundbird.model.Sound;
@@ -23,6 +27,7 @@ public class AddButtonActivity extends AppCompatActivity {
     private static final int READ_REQUEST_CODE = 42;
     MainActivity mac;
     Uri dataUri;
+    Uri tempUri;
     String buttonName= "fout";
 
     public AddButtonActivity(){
@@ -45,20 +50,36 @@ public class AddButtonActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 intent.setType("audio/*");
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                startActivityForResult(intent, READ_REQUEST_CODE);
-            }
+                startActivityForResult(intent, READ_REQUEST_CODE); }
+
         });
 
         Button saveButton = findViewById(R.id.saveButton);
+
         saveButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean big;
+                int length =1001;
+                try { InputStream inputStream = getContentResolver().openInputStream(dataUri);
+                   length = inputStream.available();
+                   System.out.println("je lengte" + length);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
                 EditText buttonNameEdit = findViewById(R.id.editText);
                 if (buttonNameEdit.length() <= 0){
                     buttonNameEdit.setError("Enter a Name");
                 } else if (buttonNameEdit.length() > 10){
                     buttonNameEdit.setError("Button Name is too long");
-                } else {
+                } else if (length<0 ||length>1021960){
+                    buttonNameEdit.setError("File is too big");
+                }
+                else {
                     buttonName = ((EditText) findViewById(R.id.editText)).getText().toString();
                     Log.i("saveButton", buttonName + dataUri);
                     if (dataUri != null) {
@@ -86,6 +107,7 @@ public class AddButtonActivity extends AppCompatActivity {
         if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             if (resultData != null) {
                  dataUri = resultData.getData();
+                 System.out.println("Dit is de dataUri"+ dataUri);
                  //buttonName = ((EditText) findViewById(R.id.editText)).getText().toString();
             }
         }
